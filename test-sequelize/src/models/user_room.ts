57340daca058a758f460f4c8401d4c6013bc7461
yu myeong cheol <mspringmspring@naver.com>
2020-06-20 03:56:56 +0900

    [MODIFY] <models/user_room.ts> user_room 모델 수정

--- test-sequelize/src/models/user_room.ts
+++ test-sequelize/src/models/user_room.ts
@@ -2,21 +2,10 @@ import { sequelize } from "../config/Connection";
 import Sequelize, { Model } from "sequelize";
 
 export class user_room extends Model<user_room> {
-    user_u_id: number;
-    room_r_id: number;
 }
 
 user_room.init(
-    {
-        user_u_id: {
-            type: Sequelize.INTEGER,
-            allowNull: false
-        },
-        room_r_id: {
-            type: Sequelize.INTEGER,
-            allowNull: false
-        }
-    },
+    {},
     {
         sequelize,
         modelName: "user_room",
