2fac364c17650024b7663a141abdc76072fee382
Yu myeong cheol <mspringmspring@naver.com>
2020-06-22 21:44:53 +0900

    [CREATE] <router/chat.router.ts> chat 라우터 생성

---
+++ test-sequelize/src/routers/chat.router.ts
@@ -0,0 +1,8 @@
+// const express = require("express");
+// const chatController = require("../controller/chat/controller");
+
+// const router = express.Router();
+
+// router.post("/chating", chatController);
+
+// export default router;
