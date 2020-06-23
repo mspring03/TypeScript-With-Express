fa41019cf4eadada7c2b4189a7102d0decdf8d4a
Yu myeong cheol <mspringmspring@naver.com>
2020-06-23 21:42:07 +0900

    [DELETE] <routers/chat.router.ts> 필요 없는 폴더

--- test-sequelize/src/routers/chat.router.ts
+++
@@ -1,8 +0,0 @@
-// const express = require("express");
-// const chatController = require("../controller/chat/controller");
-
-// const router = express.Router();
-
-// router.post("/chating", chatController);
-
-// export default router;
