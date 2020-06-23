f309f7903a1d25f802068d74815b7a7c04bcdf4f
Yu myeong cheol <mspringmspring@naver.com>
2020-06-23 22:35:40 +0900

    [MODIFY] <user/query.ts> introduce 추가

--- test-sequelize/src/controller/user/query.ts
+++ test-sequelize/src/controller/user/query.ts
@@ -11,11 +11,16 @@ const findOne = async (userId: string): Promise<Object> => {
     return user;
 }
 
-const create = async (userId: string, password: string, name: string) => {
+const create = async (userId: string, password: string, name: string, introduce: string) => {
+    const date = new Date();
+    
     await User.create({
         userId: userId,
         password: password,
         name: name,
+        introduce: introduce,
+        connection: 0,
+        last_online: `${date.getFullYear()}년/0${date.getMonth() + 1}월/${date.getDate()}일`
     });
 }
 
@@ -28,9 +33,9 @@ export const findUserById = async (userId: string): Promise<Object> => {
     }
 };
 
-export const userCreate = async (userId: string, password: string, name: string) => {
+export const userCreate = async (userId: string, password: string, name: string, introduce: string) => {
     try {
-        create(userId, password, name);
+        create(userId, password, name, introduce);
     } catch (error) {
         throw error
     }
